b6c6cf9bdaf13471cbc733db46f2fc9b663cb882
Rajat Vijay <rajatvijay5@gmail.com>
2016-04-25 18:18:44 +0530

    Added the functionality to share post using email

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail 
 
 # Create your views here.
 
@@ -11,7 +13,29 @@ class PostListView(ListView):
   paginate_by = 3
   template_name = 'blog/post/list.html'
   
-
+def post_share(request, post_id):
+  post = get_object_or_404(Post, id=post_id, status='published')
+  
+  sent = False
+  cd = None
+  
+  if request.method == 'POST':
+    # form was submitted
+    form = EmailPostForm(request.POST)
+    if form.is_valid():
+      # form fields passed validation
+      cd = form.cleaned_data
+      post_url = request.build_absolute_uri(post.get_absolute_url())
+      subject = '{0} ({1}) recommends you reading "{2}"'.format(cd['name'], cd['email'], post.title)
+      message = 'Read "{0}" at\n\n{1}\'s, comments: {2}'.format(post.title, post_url, cd['name'], cd['comments'])
+      send_mail(subject, message, 'rajatvijay5@gmail.com', [cd['to']])
+      sent = True
+      
+      # ... send mail
+  else:
+    form = EmailPostForm()
+    
+  return render(request, 'blog/post/share.html', {'form': form, 'post': post, 'sent': sent, 'cd': cd})
 
 #def post_list(request):
 #  posts_list = Post.published.all()
