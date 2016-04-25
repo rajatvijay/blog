309aedaebef081360819cf6f588926a9edca5ff2
Rajat Vijay <rajatvijay5@gmail.com>
2016-04-25 16:53:14 +0530

    Implemented class based view for post_list with paging

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,33 +1,42 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
 
-def post_list(request):
-  posts_list = Post.published.all()
-  paginator = Paginator(posts_list, 3)
+class PostListView(ListView):
+  queryset = Post.published.all()
+  context_object_name = 'posts'
+  paginate_by = 3
+  template_name = 'blog/post/list.html'
   
-  # page_number --> gets the number of current page
-  page_number = request.GET.get('page')
-  
-  try:
-    
-    # paginator.page() --> the page object having methods to retrieve objects(object_list)
-    # of page no indicated by page variable   
-    page = paginator.page(page_number)
-    
-  except PageNotAnInteger :
-    #If page_number is not an integer deliver the first page
-    page = paginator.page(1)
-  except EmptyPage:
-    # If page_number is out of range deliver last page of results
-    page = paginator.page(paginator.num_pages)
-    
-  # rather than passing a posts quesry set we pass a page object
-  # page object has all the query set objects for the given page number only 
-  # https://docs.djangoproject.com/en/1.9/topics/pagination/
-  return render(request, 'blog/post/list.html', {'page': page})
+
+
+#def post_list(request):
+#  posts_list = Post.published.all()
+#  paginator = Paginator(posts_list, 3)
+#  
+#  # page_number --> gets the number of current page
+#  page_number = request.GET.get('page')
+#  
+#  try:
+#   
+#    # paginator.page() --> the page object having methods to retrieve objects(object_list)
+#    # of page no indicated by page variable   
+#    page = paginator.page(page_number)
+#    
+#  except PageNotAnInteger :
+#    #If page_number is not an integer deliver the first page
+#    page = paginator.page(1)
+#  except EmptyPage:
+#    # If page_number is out of range deliver last page of results
+#    page = paginator.page(paginator.num_pages)
+#    
+#  # rather than passing a posts quesry set we pass a page object
+#  # page object has all the query set objects for the given page number only 
+#  # https://docs.djangoproject.com/en/1.9/topics/pagination/
+#  return render(request, 'blog/post/list.html', {'page': page})
   
   
 def post_detail(request, year, month, day, post):
